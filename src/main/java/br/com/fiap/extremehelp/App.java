50a5ab559aebecad317fd421ac0ac80117a597f8
GuiJanunzzi <guijanunzzi7@gmail.com>
2025-05-30 20:55:20 -0300

    Adicionando dependencias necessarias

--- src/main/java/br/com/fiap/extremehelp/App.java
+++ src/main/java/br/com/fiap/extremehelp/App.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ExtremehelpApplication {
+public class App {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ExtremehelpApplication.class, args);
+		SpringApplication.run(App.class, args);
 	}
 
 }
