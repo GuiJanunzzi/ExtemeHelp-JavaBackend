9586440ff6e352b862a3b1fb0a0031113d47c541
GuiJanunzzi <guijanunzzi7@gmail.com>
2025-05-30 20:49:35 -0300

    Iniciando o projeto

---
+++ src/main/java/br/com/fiap/extremehelp/ExtremehelpApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.extremehelp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtremehelpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtremehelpApplication.class, args);
+	}
+
+}
