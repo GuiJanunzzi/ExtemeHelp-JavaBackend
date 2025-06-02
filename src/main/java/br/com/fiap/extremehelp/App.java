cda5d264ffc0a12b1117374a78d143e7f9fbca6a
GuiJanunzzi <guijanunzzi7@gmail.com>
2025-06-01 21:39:19 -0300

    Adicionado documentação do swagger e  habilitando o cache no App

--- src/main/java/br/com/fiap/extremehelp/App.java
+++ src/main/java/br/com/fiap/extremehelp/App.java
@@ -2,8 +2,14 @@ package br.com.fiap.extremehelp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
+@EnableCaching
+@OpenAPIDefinition(info = @Info(title = "API do ExtremeHelp", description = "API que controla o sistema do projeto ExtremeHelp"))
 public class App {
 
 	public static void main(String[] args) {
